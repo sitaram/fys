562c2b64d0087cb41d8189e2675135f674c48144
Sitaram Iyer <sitaram@gmail.com>
2023-03-27 01:31:05 -0700

    more-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,10 @@ import Head from 'next/head'
 import { JSDOM } from 'jsdom';
 const UNSPLASH_API_KEY = '_HifaNwNoljS_lFLkUQ7L4nQulMXn6FcCazEVNlhTB8'; // TODO: move to server
 
+var initialized = false;
+var location = '';
+var preferences = '';
+
 function unsplash(container, queries) {
   function fetchImages(query) {
     var url = 'https://api.unsplash.com/search/photos?query=' + query + '&per_page=3&client_id=' + UNSPLASH_API_KEY;
@@ -20,38 +24,27 @@ function unsplash(container, queries) {
   var promises = queries.map(fetchImages);
 
   $.when.apply($, promises).then(function() {
-    var $slick = $('<div>').addClass('slick');
+    var slick = $('<div>').addClass('slick');
 
     // Loop through the results for each query and create an image element for each one
     for (var i = 0; i < arguments.length; i++) {
       var data = arguments[i][0];
-      var $tile = $('<div>');
+      var tile = $('<div>');
       $.each(data.results, function(j, result) {
 	var $img = $('<img>').addClass('slick-img').attr({
 	  'src': result.urls.thumb,
 	  'data-lazy': result.urls.regular
 	});
-	$tile.append($img);
+	tile.append($img);
       });
-      $tile.append($('<div>').addClass('bg-white shadow-md py-1 px-2 capitalize absolute slick-label').text(queries[i]));
-      $slick.append($tile);
+      tile.append($('<div>').addClass('bg-white shadow-md py-1 px-2 capitalize absolute slick-label').text(queries[i]));
+      slick.append(tile);
     }
 
     // Append the slick container to the carousel
-    container.append($slick);
+    container.append(slick);
 
-    // Initialize the slick carousel
-    if(0)
-    $slick.on('mousewheel DOMMouseScroll wheel', (function(e) {
-      e.preventDefault();
-      if (e.originalEvent.deltaY < 0) {
-	$(this).slick('slickNext');
-      } else {
-	$(this).slick('slickPrev');
-      }
-    }));
-
-    $slick.slick({
+    slick.slick({
       lazyLoad: 'ondemand',
       slidesToShow: 4,
       slidesToScroll: 2,
@@ -59,7 +52,7 @@ function unsplash(container, queries) {
       arrows: true,
       prevArrow:"<img class='ml-4 z-40 slick-prev' src='/left.png'>",
       nextArrow:"<img class='mr-4 z-40 slick-next' src='/right.png'>",
-      dots: true,
+      // dots: true,
       responsive: [
 	{
 	  breakpoint: 1000,
@@ -118,26 +111,28 @@ const render = (data) => {
   $box.append($carousel);
 
   $box.append(`
-      Select one of the above to:
-      <div class="radio-buttons inline-flex">
+      Select one above to:
+      <div class="radio-buttons inline-flex py-2">
 	<input type="radio" id="option1" name="radio" value="Option 1" class="form-radio sr-only" />
-	<label htmlFor="option1" class="radio-label-text px-2 py-1 ml-2 cursor-pointer rounded-sm">Look deeper</label>
+	<label for="option1" class="radio-label-text px-1 cursor-pointer rounded-sm">Look deeper</label> |
+
 	<input type="radio" id="option2" name="radio" value="Option 2" class="form-radio sr-only" />
-	<label htmlFor="option2" class="radio-label-text px-2 py-1 ml-2 cursor-pointer rounded-sm">See similar</label>
+	<label for="option2" class="radio-label-text px-1 cursor-pointer rounded-sm">See similar</label>
       </div>
 
       <br>Or add preferences:
       <input type="text" size=30 placeholder="E.g., with kids, hiking, near palo alto" class="border-2 my-2 border-gray-200 rounded-sm py-1 px-2 focus:outline-none focus:border-blue-300 focus:shadow-md">
-      E.g.,
-      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="with kids" />
-      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="active" />
-      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="indoor" />
+      <br>E.g.,
+      <input type="button" class="px-2 border-0 border-gray-100 focus:bg-blue-500 focus:text-white" value="near ` + location + `" />
+      <input type="button" class="px-2 border-0 border-gray-100 focus:bg-blue-500 focus:text-white" value="with kids" />
+      <input type="button" class="px-2 border-0 border-gray-100 focus:bg-blue-500 focus:text-white" value="active" />
+      <input type="button" class="px-2 border-0 border-gray-100 focus:bg-blue-500 focus:text-white" value="indoor" />
   `);
 
   $('#rest').append($box);
 
-  $('.radio-buttons').each(function() { $(this).children().first().attr('checked', true); });
-  $('input[type="radio"]').click(function() {
+  $('.radio-buttons').each(function() { $(this).children().first().attr('checked', true); }); // XXX clears all but latest
+  $('input[type="radio"]').click(function(e) {  // XXX scrolls to top
     console.log(this);
   });
 
@@ -165,34 +160,29 @@ const openai = (prompt) => {
   });
 };
 
+function showPosition(position) {
+  var latitude = position.coords.latitude;
+  var longitude = position.coords.longitude;
+  var accuracy = position.coords.accuracy;
+  var url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=b6640a4ba829450ea55332d76e588fdc&no_annotations=1";
+  $.get(url, function(data) {
+    var res = data.results[0].components;
+    location = res.city + ', ' + res.state_code || res.state;
+    $("#location").html('&#x25cf; ' + location);
+  });
+}
 
 export default function Home() {
   useEffect(() => {
     $(document).ready(() => {
+	if (initialized) return;
+	initialized = true;
 
-	function getLocation() {
-	  if (navigator.geolocation) {
-	    navigator.geolocation.getCurrentPosition(showPosition);
-	  } else {
-	    alert("Geolocation is not supported by this browser.");
-	  }
-	}
-	getLocation();
-
-	function showPosition(position) {
-	  var latitude = position.coords.latitude;
-	  var longitude = position.coords.longitude;
-	  var accuracy = position.coords.accuracy;
-	  var url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=b6640a4ba829450ea55332d76e588fdc&no_annotations=1";
-	  $.get(url, function(data) {
-	    var res = data.results[0].components;
-	    var loc = res.city + ', ' + res.state_code || res.state;
-	    $("#location").html('&#x25cf; ' + loc);
-	  });
-	}
+	if (navigator.geolocation)
+	  navigator.geolocation.getCurrentPosition(showPosition);
 
 	$('#weekend').off('click').click(function(e) {
-	  openai(`Bulleted list of 15 popular and diverse activities that people do over the weekend.`);
+	  openai(`Bulleted list of 15 popular and diverse activities that people do over the weekend, ` + preferences + `.`);
 	});
 
     });
@@ -208,13 +198,13 @@ export default function Home() {
       </Head>
       <main className="p-3">
 
+	<div id="location" className="fixed top-5 right-5 text-orange-700 text-sm font-bold"></div>
+
 	<div id="overlay"><div className="cv-spinner"><span className="spinner"></span></div></div>
 
-	<div id="weekend" className="relative p-3 max-w-sm bg-white
+	<div id="weekend" className="p-3 max-w-sm bg-white
 	  rounded-lg flex items-center space-x-4 shadow-md hover:shadow-xl
 	  active:shadow-none active:bg-neutral-100 active:mt-0.5 active:-mb-0.5 active:ml-0.5 active:-mr-0.5 ">
-
-	  <div id="location" className="absolute top-1 right-1 text-orange-700 text-sm font-bold"></div>
 
 	  <img className="h-23 w-28 py-2" src="/weekend.png" alt="Weekend" />
 	  <div>
@@ -222,7 +212,6 @@ export default function Home() {
 	    <p className="text-xl text-black">this weekend?</p>
 	  </div>
 	</div>
-
 
 	<div id="rest"></div>
       </main>

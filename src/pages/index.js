251686a3437850b70626289d1d1f411c0f922647
Sitaram Iyer <sitaram@gmail.com>
2023-03-27 00:41:03 -0700

    more-ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
-import $ from 'jquery';
+import $ from "jquery";
+
 if (typeof window !== 'undefined')
   import('slick-carousel');  // load dynamically if running on the client side
 
@@ -70,7 +71,7 @@ function unsplash(container, queries) {
 	{
 	  breakpoint: 700,
 	  settings: {
-	    slidesToShow: 2,
+	    slidesToShow: 1,
 	    slidesToScroll: 2,
 	  }
 	},
@@ -113,8 +114,36 @@ const render = (data) => {
     map(x => capitalize(x.replace(/^ *(\d+\.)? */,'').replace(/\.$/,''))));
 
   var $box = $('<div class="p-3 my-2 bg-white rounded-lg shadow-md">').text('Here are some initial ideas:');
+  var $carousel = $('<div>');
+  $box.append($carousel);
+
+  $box.append(`
+      Select one of the above to:
+      <div class="radio-buttons inline-flex">
+	<input type="radio" id="option1" name="radio" value="Option 1" class="form-radio sr-only" />
+	<label htmlFor="option1" class="radio-label-text px-2 py-1 ml-2 cursor-pointer rounded-sm">Look deeper</label>
+	<input type="radio" id="option2" name="radio" value="Option 2" class="form-radio sr-only" />
+	<label htmlFor="option2" class="radio-label-text px-2 py-1 ml-2 cursor-pointer rounded-sm">See similar</label>
+      </div>
+
+      <br>Or add preferences:
+      <input type="text" size=30 placeholder="E.g., with kids, hiking, near palo alto" class="border-2 my-2 border-gray-200 rounded-sm py-1 px-2 focus:outline-none focus:border-blue-300 focus:shadow-md">
+      <div>
+      E.g.,
+      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="with kids" />
+      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="active" />
+      <input type="button" class="px-2 border-2 border-gray-100 active:bg-blue-500 active:text-white" value="indoor" />
+      </div>
+  `);
+
   $('#rest').append($box);
-  unsplash($box, list[0]);
+
+  $('.radio-buttons').each(function() { $(this).children().first().attr('checked', true); });
+  $('input[type="radio"]').click(function() {
+    console.log(this);
+  });
+
+  unsplash($carousel, list[0]);
 };
 
 const openai = (prompt) => {
@@ -143,6 +172,27 @@ export default function Home() {
   useEffect(() => {
     $(document).ready(() => {
 
+	function getLocation() {
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(showPosition);
+	  } else {
+	    alert("Geolocation is not supported by this browser.");
+	  }
+	}
+	getLocation();
+
+	function showPosition(position) {
+	  var latitude = position.coords.latitude;
+	  var longitude = position.coords.longitude;
+	  var accuracy = position.coords.accuracy;
+	  var url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=b6640a4ba829450ea55332d76e588fdc&no_annotations=1";
+	  $.get(url, function(data) {
+	    var res = data.results[0].components;
+	    var loc = res.city + ', ' + res.state_code || res.state;
+	    $("#location").html('&#x25cf; ' + loc);
+	  });
+	}
+
 	$('#weekend').off('click').click(function(e) {
 	  openai(`Bulleted list of 15 popular and diverse activities that people do over the weekend.`);
 	});
@@ -162,16 +212,19 @@ export default function Home() {
 
 	<div id="overlay"><div className="cv-spinner"><span className="spinner"></span></div></div>
 
-	<div id="weekend" className="p-3 max-w-sm bg-white
+	<div id="weekend" className="relative p-3 max-w-sm bg-white
 	  rounded-lg flex items-center space-x-4 shadow-md hover:shadow-xl
 	  active:shadow-none active:bg-neutral-100 active:mt-0.5 active:-mb-0.5 active:ml-0.5 active:-mr-0.5 ">
 
-	  <img className="h-24 w-30" src="/weekend.png" alt="Weekend" />
+	  <div id="location" className="absolute top-1 right-1 text-orange-700 text-sm font-bold"></div>
+
+	  <img className="h-23 w-28 py-2" src="/weekend.png" alt="Weekend" />
 	  <div>
 	    <div className="text-l text-slate-500">What do you want to do..</div>
 	    <p className="text-xl text-black">this weekend?</p>
 	  </div>
 	</div>
+
 
 	<div id="rest"></div>
       </main>

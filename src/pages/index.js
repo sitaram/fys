562f59ff45fab6c7d293dd09cf35b9bfb6a63bbe
Sitaram Iyer <sitaram@gmail.com>
2023-04-14 13:15:46 -0700

    oakland-default

--- src/pages/index.js
+++ src/pages/index.js
@@ -213,6 +213,7 @@ function showPosition(position) {
   var longitude = position.coords.longitude;
   var accuracy = position.coords.accuracy;
   var url = "https://api.opencagedata.com/geocode/v1/json?q=" + latitude + "+" + longitude + "&key=b6640a4ba829450ea55332d76e588fdc&no_annotations=1";
+  location = "Oakland, CA";  // default here if not found
   $.get(url, function(data) {
     var res = data.results[0].components;
     location = res.city + ', ' + res.state_code || res.state;
